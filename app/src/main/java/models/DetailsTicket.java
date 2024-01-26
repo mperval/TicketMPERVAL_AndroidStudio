20808c88a02bfe6e454c5d58b86eb06b1528811c
mperval <mperval735@iescarrillo.es>
2024-01-26 14:52:39 +0100

    v

--- app/src/main/java/models/DetailsTicket.java
+++ app/src/main/java/models/DetailsTicket.java
@@ -5,6 +5,12 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class DetailsTicket implements Serializable {
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("amount")
+    @Expose
+    private String amount;
     @SerializedName("id")
     @Expose
     private String id;
